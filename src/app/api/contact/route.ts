56a17dfdafc190dee859cb101ece8275f380f89e
Azz-eddine Bouali <azzdinne.bouali@gmail.com>
2023-07-26 11:19:59 +0100

    fix:route deleted

--- src/app/api/contact/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server';
-import dbConnect from '@/lib/db';
-import Message from '@/lib/message';
-export async function POST(req: NextRequest, res: NextResponse) {
-  console.log('req.body', req.body);
-  return res.json({ message: 'ok' });
-}
